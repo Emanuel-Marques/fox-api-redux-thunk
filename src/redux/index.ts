18b43ae0d7dc7d426b93fc84e15abb84f6b526b0
Emanuel Marques <emanuelmarques585@gmail.com>
2023-12-10 14:58:16 +0100

    Ajustes finais

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,5 +1,5 @@
 import { legacy_createStore as createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 
 import gallery from './reducers/gallery';
 
